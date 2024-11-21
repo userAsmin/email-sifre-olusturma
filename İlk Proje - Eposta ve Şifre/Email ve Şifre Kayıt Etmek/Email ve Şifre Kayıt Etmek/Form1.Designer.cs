1b8b698101556e5ea6b780b5c47b4d12f100d188
asmin <asminzzzz0@gmail.com>
2024-11-21 23:29:04 +0300

    Add files via upload

---
+++ İlk Proje - Eposta ve Şifre/Email ve Şifre Kayıt Etmek/Email ve Şifre Kayıt Etmek/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Email_ve_Şifre_Kayıt_Etmek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnKayit = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtGirisMail = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtGirisSifre = new System.Windows.Forms.TextBox();
+            this.btnGirisYap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(33, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EMAİL ADRESİNİZİ GİRİNİZ.\r\n\r\n";
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(36, 101);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(171, 20);
+            this.txtMail.TabIndex = 1;
+            this.txtMail.Text = "@gmail.com";
+            this.txtMail.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(13, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "KAYIT EKRANI";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(33, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 26);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "ŞİFRE OLUŞTURUNUZ\r\n\r\n";
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(36, 169);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(171, 20);
+            this.txtSifre.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(212, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 69);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Şifre Oluşturma Şartları;\r\n· İngilizce harf kullanımı.\r\n· En az 8 karakteri\r\n· En" +
+    " az bir adet \". (nokta)\", \"?\", \"_\", \"%\" ifadesi kullanımı.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(374, -25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(10, 468);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n" +
+    "|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n";
+            // 
+            // btnKayit
+            // 
+            this.btnKayit.Location = new System.Drawing.Point(35, 215);
+            this.btnKayit.Name = "btnKayit";
+            this.btnKayit.Size = new System.Drawing.Size(104, 36);
+            this.btnKayit.TabIndex = 6;
+            this.btnKayit.Text = "Hesap Oluştur";
+            this.btnKayit.UseVisualStyleBackColor = true;
+            this.btnKayit.Click += new System.EventHandler(this.btnKayit_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(415, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(210, 31);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "GİRİŞ EKRANI";
+            // 
+            // txtGirisMail
+            // 
+            this.txtGirisMail.Location = new System.Drawing.Point(437, 101);
+            this.txtGirisMail.Name = "txtGirisMail";
+            this.txtGirisMail.Size = new System.Drawing.Size(171, 20);
+            this.txtGirisMail.TabIndex = 1;
+            this.txtGirisMail.Text = "@gmail.com";
+            this.txtGirisMail.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(434, 85);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(225, 26);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "KAYITLI EMAİL ADRESİNİZİ GİRİNİZ.\r\n\r\n";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(434, 152);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(125, 39);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "ŞİFRENİZİ YAZINIZ.\r\n\r\n\r\n";
+            // 
+            // txtGirisSifre
+            // 
+            this.txtGirisSifre.Location = new System.Drawing.Point(437, 169);
+            this.txtGirisSifre.Name = "txtGirisSifre";
+            this.txtGirisSifre.Size = new System.Drawing.Size(171, 20);
+            this.txtGirisSifre.TabIndex = 3;
+            // 
+            // btnGirisYap
+            // 
+            this.btnGirisYap.Location = new System.Drawing.Point(437, 215);
+            this.btnGirisYap.Name = "btnGirisYap";
+            this.btnGirisYap.Size = new System.Drawing.Size(104, 36);
+            this.btnGirisYap.TabIndex = 6;
+            this.btnGirisYap.Text = "Giriş Yap";
+            this.btnGirisYap.UseVisualStyleBackColor = true;
+            this.btnGirisYap.Click += new System.EventHandler(this.btnGirisYap_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 301);
+            this.Controls.Add(this.btnGirisYap);
+            this.Controls.Add(this.btnKayit);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtGirisSifre);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtGirisMail);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnKayit;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtGirisMail;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtGirisSifre;
+        private System.Windows.Forms.Button btnGirisYap;
+    }
+}
+
---
+++ İlk Proje - Eposta ve Şifre/Email ve Şifre Kayıt Etmek/Email ve Şifre Kayıt Etmek/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace Email_ve_Şifre_Kayıt_Etmek
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> kullaniciVerileri = new Dictionary<string, string>();
+        public Form1()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void btnKayit_Click(object sender, EventArgs e)
+        {
+            string KayitMail = txtMail.Text;
+            string KayitSifre = txtSifre.Text;
+            string[] isaretler = { ".", "?", "_", "%" };
+            string[] trkKarakter = { "İ", "ı", "ö", "Ö", "ü", "Ü", "ç", "Ç", "ğ", "Ğ", "ş", "Ş" };
+
+
+            foreach (char karakter in KayitSifre)
+            {
+                if (trkKarakter.Contains(karakter.ToString()))
+                {
+                    MessageBox.Show("Uygun olmayan karakter girdiniz");
+                    return;
+                }
+            }
+            foreach (char karakter in KayitSifre)
+            {
+                if (!char.IsLetterOrDigit(karakter) && !isaretler.Contains(karakter.ToString()))
+                {
+                    MessageBox.Show("Uygun olmayan karakter girdiniz");
+                    return;
+                }
+            }
+            
+
+                if (KayitSifre.Length < 8)
+                {
+                MessageBox.Show("8 veya daha fazla karakter giriniz");
+                }
+            if (!kullaniciVerileri.ContainsKey(KayitMail))
+            {
+                kullaniciVerileri.Add(KayitMail, KayitSifre);
+                MessageBox.Show("Hesap başarıyla oluşturuldu!");
+            }
+            else
+            {
+                MessageBox.Show("Bu e-posta adresi zaten kayıtlı.");
+            }
+        }
+
+        private void btnGirisYap_Click(object sender, EventArgs e)
+        {
+            string GirisMail = txtGirisMail.Text;
+            string GirisSifre = txtGirisSifre.Text;
+
+            if (kullaniciVerileri.ContainsKey(GirisMail) && kullaniciVerileri[GirisMail] == GirisSifre)
+            {
+                MessageBox.Show("Giriş başarılı!");
+            }
+            else
+            {
+                MessageBox.Show("E-posta veya şifre hatalı!");
+            }
+        }
+    }
+}
+
+
+        
+
+
+
